a674a428b289e4626c3a33579f6787ddd08b5ba7
r4v3nx <131524710+r4v3nx@users.noreply.github.com>
2023-09-17 12:06:12 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.51.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file("jsonfile")
+  project     = "project_name"
+  region      = "us-east1"
+  zone        = "us-east1-b"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "jenkins"
+  machine_type = "e2-small"
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {
+      //Ephemeral public IP
+    }
+}
+}
